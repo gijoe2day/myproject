cd720098243e6af7b8e1f3beed226933206caacd
gijoe2day <gjuliao32@hotmail.com>
2022-11-16 17:40:19 -0500

    Finished adding javascript and styling popups

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 function on() {
   document.getElementById("overlay").style.display = "block";
 }
- function off() {
+
+function off() {
   document.getElementById("overlay").style.display = "none";
 }
-
 
 /* Dynamic Project Functions */
 function projectOn() {
@@ -17,53 +17,47 @@ function projectOff() {
 }
 
 
-function getProjectOne() {
+
+function getProject(a) {
   projectOn();
 fetch("projects.json")
 .then((res) => res.json())
 .then((data) => {
  output = `
- <!--Empty div for the overley effect-->
  <div class="project_overlay">
- </div>
- 
   <div class="project_structure_overlay">
-  <div class="project_header"> 
-      
+      <div> 
+          <button onclick="projectOff()" class="project_overlay_btn_close"> X </button>
+      </div>
       <div class="project_heading_overlay">
-              <h2 class="title_overlay"> ${data[0].name} </h2>
+              <h2 class="title_overlay"> ${data[a].name} </h2>
               <ul class="project_body_title_list">
-                  <li class="title_list_main_overlay">${data[0].projectClient}</li>
-                  <img src="./Assets/Counter.png" alt="" style="margin-right:3%">
-                  <li>${data[0].projectInfo[0]}</li>
-                  <img src="./Assets/Counter.png" alt="" style="margin-right:3%">
-                  <li>${data[0].projectInfo[1]}</li>
+                  <li class="title_list_main_overlay">${data[a].projectClient}</li>
+                  <li><img class="counter_overlay" src="./Assets/Counter.png" >${data[a].projectInfo[0]}</li>
+                  <li><img class="counter_overlay" src="./Assets/Counter.png" >${data[a].projectInfo[1]}</li>
               </ul>
       </div>
-      <div>
-      <button onclick="projectOff()">X</button>
-  </div>
-
-      </div>
-<div> <img class="image_ovlerlay" src=${data[0].image} alt="snapshoot"> </div>  
+      <div> <img class="image_ovlerlay" src=${data[a].image} alt="snapshoot"> </div>
       <div class="project_body_overlay">
           <div class="text_group_overlay">
-              <p>${data[0].mainText}</p>
+              <p>${data[a].mainText}</p>
           </div>
-              <hr>
           <div class="btn_group_overlay">
           <ul class="project_tech_overlay">
-                  <li>${data[0].technologies[0]}</li>
-                  <li>${data[0].technologies[1]}</li>
-                  <li>${data[0].technologies[2]}</li>
+                  <li>${data[a].technologies[0]}</li>
+                  <li>${data[a].technologies[1]}</li>
+                  <li>${data[a].technologies[2]}</li>
               </ul>
-              <button onclick=window.open(${data[0].liveVersionLink[1]}) class="overlay_btn"> See Live <img src="./Assets/github.svg" > </button>
-              <button onclick=window.open(${data[0].linkToSource[1]}) class="overlay_btn"> See Source <img src="./Assets/github.svg" > </button>
+              <hr class="hr_style">
+              <div class="btn_flex_overlay">
+                <button class="overlay_btn"> <a href="${data[a].liveVersionLink}"> See Live <img src="./Assets/Icon_Export.png" > </a> </button>
+                <button class="overlay_btn"> <a href="${data[a].liveVersionLink}"> See Source <img src="./Assets/github_in_button.png" > </a> </button>
+              </div>
           </div>
       </div>
- 
+  
   </div>
-`;
+  </div>`; 
 
   document.getElementById("output").innerHTML = output;
 });
